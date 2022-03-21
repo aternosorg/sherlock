b6a8f5fd8f41d91cf2f5ca8d9bc66a8c1fd64e96
Julian <julian@aternos.org>
2022-03-21 17:16:47 +0100

    simplify if-structure in ObfuscatedString

--- src/ObfuscatedString.php
+++ src/ObfuscatedString.php
@@ -22,34 +22,40 @@ class ObfuscatedString
 
     public function getMappedContent(): string
     {
-        if ($this->deobfuscatedContent === null) {
-            $this->deobfuscatedContent = "";
-            foreach (preg_split("/((\r?\n)|(\r\n?))/", $this->content) as $line) {
-                if (preg_match("/^(\s*)at (.+)\.([^.]+)\(([^:]+):(\d+)\)(.*)$/", $line, $matches)) {
-                    [,$whitespace, $className, $methodName, $pretences, $lineNumber, $end] = $matches;
-                    $class = $this->obfuscationMap->getClass($className);
-                    if ($class !== null) {
-                        $method = $this->obfuscationMap->getMethod($className, $methodName, $lineNumber);
-                        if ($method !== null) {
-                            $methodName = $method->getName();
-                        }
+        if ($this->deobfuscatedContent !== null) {
+            return $this->deobfuscatedContent;
+        }
 
-                        // client/optifine? file names
-                        if (preg_match("/^\s+\[$className\.class:\?]$/", $end)) {
-                            $end = " [" . $class->getName() . ".class:?]";
-                        }
-
-                        //Fabric file names
-                        if ("net.minecraft." . $pretences === $class->getUnmappedName()  . ".java") {
-                            $pretences = $class->getName() . ".java";
-                        }
-
-                        $this->deobfuscatedContent .= "${whitespace}at " . $class->getPath() . ".$methodName($pretences:$lineNumber)$end\n";
-                        continue;
-                    }
-                }
+        $this->deobfuscatedContent = "";
+        foreach (preg_split("/((\r?\n)|(\r\n?))/", $this->content) as $line) {
+            if (!preg_match("/^(\s*)at (.+)\.([^.]+)\(([^:]+):(\d+)\)(.*)$/", $line, $matches)) {
                 $this->deobfuscatedContent .= $line . "\n";
+                continue;
             }
+
+            [, $whitespace, $className, $methodName, $pretences, $lineNumber, $end] = $matches;
+            $class = $this->obfuscationMap->getClass($className);
+            if ($class === null) {
+                $this->deobfuscatedContent .= $line . "\n";
+                continue;
+            }
+
+            $method = $this->obfuscationMap->getMethod($className, $methodName, $lineNumber);
+            if ($method !== null) {
+                $methodName = $method->getName();
+            }
+
+            // client/optifine? file names
+            if (preg_match("/^\s+\[$className\.class:\?]$/", $end)) {
+                $end = " [" . $class->getName() . ".class:?]";
+            }
+
+            //Fabric file names
+            if ("net.minecraft." . $pretences === $class->getUnmappedName() . ".java") {
+                $pretences = $class->getName() . ".java";
+            }
+
+            $this->deobfuscatedContent .= "${whitespace}at " . $class->getPath() . ".$methodName($pretences:$lineNumber)$end\n";
         }
 
         return $this->deobfuscatedContent;
