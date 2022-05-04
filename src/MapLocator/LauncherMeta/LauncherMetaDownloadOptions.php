463d97993a47f9bd773b78e03e43e54973447cec
Julian <julian@aternos.org>
2022-05-04 17:13:11 +0200

    allow mappings to be null

--- src/MapLocator/LauncherMeta/LauncherMetaDownloadOptions.php
+++ src/MapLocator/LauncherMeta/LauncherMetaDownloadOptions.php
@@ -5,23 +5,27 @@ namespace Aternos\Sherlock\MapLocator\LauncherMeta;
 class LauncherMetaDownloadOptions
 {
     protected LauncherMetaFile $client;
-    protected LauncherMetaFile $client_mappings;
+    protected ?LauncherMetaFile $client_mappings = null;
     protected LauncherMetaFile $server;
-    protected LauncherMetaFile $server_mappings;
+    protected ?LauncherMetaFile $server_mappings = null;
 
     public function __construct($data)
     {
         $this->client = new LauncherMetaFile($data->client);
-        $this->client_mappings = new LauncherMetaFile($data->client_mappings);
+        if (isset($data->client_mappings)) {
+            $this->client_mappings = new LauncherMetaFile($data->client_mappings);
+        }
         $this->server = new LauncherMetaFile($data->server);
-        $this->server_mappings = new LauncherMetaFile($data->server_mappings);
+        if (isset($data->server_mappings)) {
+            $this->server_mappings = new LauncherMetaFile($data->server_mappings);
+        }
     }
 
     /**
      * @param string $type server or client
-     * @return LauncherMetaFile
+     * @return ?LauncherMetaFile
      */
-    public function getMappings(string $type): LauncherMetaFile
+    public function getMappings(string $type): ?LauncherMetaFile
     {
         return match ($type) {
             "server", "server_mappings" => $this->server_mappings,
